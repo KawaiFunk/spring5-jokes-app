4545681e91c64a74da04462ea593aff874131208
Damian Dan <dan.damian@isa.utm.md>
2024-09-05 13:35:16 +0300

    Initial

---
+++ src/test/java/guru/springframework/spring5_jokes_app/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5_jokes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
